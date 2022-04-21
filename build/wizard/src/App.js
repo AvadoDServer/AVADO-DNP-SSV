5368c34fd0d8851787649414dcb8e05c80f3f1ef
Norman Saade <norman.saade@gmail.com>
2022-04-21 12:31:19 +0200

    react app start

---
+++ build/wizard/src/App.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from 'react';
+import "./css/style.sass";
+import axios from "axios";
+import Validators from './Validators';
+
+function App() {
+    const [authToken, setAuthToken] = React.useState(true);
+
+    React.useEffect(() => {
+        axios.get("./auth-token.txt").then((res) => {
+            setAuthToken(res.data);
+        })
+    }, []);
+
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+            console.log("Make sure you have metamask!");
+            return;
+            } else {
+            console.log("We have the ethereum object", ethereum);
+            }
+
+            /*
+            * Check if we're authorized to access the user's wallet
+            */
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+
+            if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found an authorized account:", account);
+            setCurrentAccount(account);
+            } else {
+            console.log("No authorized account found");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+            alert("Get MetaMask!");
+            return;
+            }
+
+            const accounts = await ethereum.request({
+            method: "eth_requestAccounts",
+            });
+
+            console.log("Connected", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    return (
+        <div className="App">
+            <section className="hero is-default is-bold">
+                <div className="hero-body">
+                    <div className="container has-text-centered ">
+                        <div className="columns is-vcentered">
+                            <div className="column  is-6 is-offset-1">
+                                <h1 className="title has-text-white is-2">
+                                    Avado SSV
+                                </h1>
+                                <br />
+                                <h2 className="subtitle  has-text-white is-4">
+                                    Let's start by registering your ssv node
+                                </h2>
+
+
+                                <p className="has-text-centered has-text-white">
+                                    tbd<br />
+                                </p><br />
+                                <p className="has-text-centered has-text-white">
+                                   tbd
+                                </p>
+                                <br />
+                                <br />
+                                {!currentAccount && (
+                                    <p className="has-text-centered">
+                                        <button className="button is-medium is-link" onClick={connectWallet}>
+                                            connect metamask wallet
+                                        </button>
+                                    </p>
+                                )}
+                                {currentAccount && (
+                                    <p className="has-text-centered">
+                                        <a className="button is-medium is-link" target="_blank">
+                                            register
+                                        </a>
+                                    </p>
+                                )}
+                            </div>
+                            <Validators network="mainnet" apiToken={authToken} />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default App;
